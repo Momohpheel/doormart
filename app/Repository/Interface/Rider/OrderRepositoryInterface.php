ca67284fc7877e248864bfd99f3b4bbefe25dd01
Momohpheel <momohmayowa14@gmail.com>
2022-09-13 17:12:57 +0100

    update

--- app/Repository/Interface/Rider/OrderRepositoryInterface.php
+++ app/Repository/Interface/Rider/OrderRepositoryInterface.php
@@ -10,11 +10,13 @@ interface OrderRepositoryInterface{
     public function getAllRequestedOrders();
     public function getAllRiderOrders();
     public function getAllCompletedtedOrders();
-    public function getOpenOrders();
+    // public function getOpenOrders();
     public function dashboard();
     public function getTransactionHistory();
     public function acceptOrder(string $orderId);
-    public function declineOrder();
+    public function userReceivedOrder(string $id);
+    public function orderArrived(string $id);
+    public function receiveOrder(string $id);
 
 
 
